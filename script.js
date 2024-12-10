eacb9abe2816207ea30244f67bb9e54b8099c07e
geaous2425 <geaous2425@gmail.com>
2024-12-10 19:34:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,4 +6,13 @@ document.addEventListener('DOMContentLoaded', () => {
             alert('Gracias por tu compra 🎉');
         });
     });
+    function performSearch() {
+    const query = document.getElementById("search-bar").value;
+    if (query) {
+        document.getElementById("search-results").innerHTML = `Resultados para: <strong>${query}</strong>`;
+    } else {
+        document.getElementById("search-results").innerHTML = "Por favor, introduce un término de búsqueda.";
+    }
+}
+
 });
